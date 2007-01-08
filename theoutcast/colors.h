41f2241acc219b10af28310a74a6987b7dbf4eb6
Ivan Vučica <ivan@vucica.net>
2007-01-08 19:40:11 +0000

    colors.h >: (
    git-svn-id: svn://ivan.vucica.net/theoutcast@9 4617c47b-1226-0410-b705-8e5dc674c1d5

---
+++ theoutcast/colors.h
@@ -0,0 +1,19 @@
+// tibia colors
+
+#ifndef __COLORS_H
+#define __COLORS_H
+
+extern float colors[];
+
+enum consolecolors {
+	CONRED = 0,
+	CONBLUE = 1,
+	CONYELLOW = 2,
+	CONORANGE = 3,
+	CONWHITE = 4,
+	CONLTBLUE = 5,
+	CONGREEN = 6
+};
+
+
+#endif
